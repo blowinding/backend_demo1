60480cfb4fc4145528869efc3c8eed5653b1bf0b
libtity <97455139+libtity@users.noreply.github.com>
2024-07-03 16:45:30 +0800

    libtity

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -8,6 +8,8 @@ public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
+        System.out.println("libtity push");
     }
+
 
 }
